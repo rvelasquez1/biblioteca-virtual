ab25b86f600b159a57fd7417f2eff8c1340e2d2b
rvelasquez1 <rvelasquezclaure@gmail.com>
2022-08-26 12:45:22 -0400

    practica1 en clases

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public Autor (){
+	// write your code here
+    }
+    public Autor( String nombre,String primerApellido){
+            this.nombre=nombre;
+            this.primerApellido;
+
+        }
+        public Autor(String nombre,String primerApellido,String segundoApellido){
+            this nombre=nombre;
+            this.primerApellido;
+            this.segundoApelido;
+        }
+}
